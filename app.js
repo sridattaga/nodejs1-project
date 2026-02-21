e1d07d71fd3e08afb84cb9aa07542be2eb4ccddf
lakshmansrikanth123 <lakshmansrikanthkorla.225@gmail.com>
2026-02-21 23:00:34 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+// Simple Node.js application
+
+console.log("Hello from Node.js Sample Project!");
+
+// Example function
+function add(a, b) {
+    return a + b;
+}
+
+// Call function
+const result = add(5, 3);
+console.log("Addition result:", result);
